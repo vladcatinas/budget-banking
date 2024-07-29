26efb1f17874b8c6a1ffcf73fec522172800055e
DESKTOP-2MHA8QI\pc <vladccatinas@gmail.com>
2024-07-29 11:11:48 +0300

    Partial implementation of sidebars. left is done, right remaining

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,39 @@
+'use client';
+
+import { sidebarLinks } from '@/constants';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import MobileNav from '@/components/MobileNav';
+import Sidebar from '@/components/Sidebar';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: 'Vlad', lastName: 'JSM' };
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-scren w-full font-inter">
+        <Sidebar user={loggedIn} />
+
+        <div className="flex size-full flex-col">
+
+          <div className="root-layout"> 
+
+            <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+            <div> 
+                <MobileNav user = {loggedIn} />
+            </div>
+          </div>
+          {children}
+        </div>
+
+        
     </main>
   );
 }
---
+++ components/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightSidebar = ({user, transactions, banks}:RightSidebarProps) => {
+  return (
+    <aside className="right-sidebar"> 
+        
+    </aside>
+  )
+}
+
+export default RightSidebar
